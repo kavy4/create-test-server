4e673fd79e57e4b6a5c789add50d926755f63c32
kavy4 <tyty020408@gmail.com>
2023-01-10 17:27:42 +0300

    edited useCors

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const PORT = 5000
 const botSettings = require('./file/bot.json')
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: '*'
+}))
 
 app.post('/api/send-test-to-telegram', (request, response) => {
     const text = `новый тест: <code>${JSON.stringify(request.body)}</code>`
